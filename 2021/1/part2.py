65f021f537b544d3c5736f3e456e7b2ff25a9772
Connor Lucier <connor@connorlucier.com>
2021-12-25 16:04:55 -0800

    simpler day 1 part 2 solution

--- 2021/1/part2.py
+++ 2021/1/part2.py
@@ -2,7 +2,7 @@ def part2(filename):
 	with open(filename, 'r') as file:
 		data = [int(l) for l in file.read().splitlines()]
 
-	return sum([1 if sum(data[i:i+3]) > sum(data[i-1:i+2]) else 0 for i in range(1, len(data) - 2)])
+	return sum([1 if data[i+2] > data[i-1] else 0 for i in range(1, len(data) - 2)])
 
 if __name__ == '__main__':
 	count = part2('input.txt')
