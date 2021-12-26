755fcb7c9b2d0ecaae79ed0aab762ce2a3101384
Connor Lucier <connor@connorlucier.com>
2021-12-26 11:38:18 -0800

    day 7 part 2 complete
    gotta love a good math trick

--- 2021/7/part2.py
+++ 2021/7/part2.py
@@ -1,4 +1,4 @@
-def part1(filename):
+def part2(filename):
 	with open(filename, 'r') as file:
 		data = [int(val) for val in file.read().split(',')]
 
@@ -7,10 +7,12 @@ def part1(filename):
 
 	fuel_to_pos = [0] * (max_pos - min_pos)
 	for i in range(len(fuel_to_pos)):
-		fuel_to_pos[i] = sum([abs(val - i) for val in data])
+		for val in data:
+			steps_to_pos = abs(val - i)
+			fuel_to_pos[i] += steps_to_pos * (steps_to_pos + 1) // 2
 
 	return min(fuel_to_pos)
 
 if __name__ == '__main__':
-	result = part1('input.txt')
+	result = part2('input.txt')
 	print(result)
