fdb530816b13525fb082262c221eb55deba0556d
Connor Lucier <connor@connorlucier.com>
2024-11-30 21:41:49 -0800

    2024 day 1 part 2

--- 2024/01/part2.go
+++ 2024/01/part2.go
@@ -1,9 +1,39 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"strconv"
+	"strings"
+	"os"
 )
 
 func part2() {
-	fmt.Println("Hello World!")
+	file, _ := os.Open("input.txt")
+	scanner := bufio.NewScanner(file)
+	defer file.Close()
+
+	left := make([]int, 1)
+	right := make([]int, 1)
+	for scanner.Scan() {
+		vals := strings.Fields(scanner.Text())
+		lval, _ := strconv.Atoi(vals[0])
+		rval, _ := strconv.Atoi(vals[1])
+		left = append(left, lval)
+		right = append(right, rval)
+	}
+
+	result := 0
+	for _, lval := range left {
+		count := 0
+		for _, rval := range right {
+			if lval == rval {
+				count++
+			}
+		}
+
+		result += lval * count
+	}
+
+	fmt.Println(result)
 }
